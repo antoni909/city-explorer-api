19aa33f6b888da9713d2bc3d63be7c5e398df1d3
antoni <lorenzo.antonio.090@gmail.com>
2021-04-12 20:59:51 -0700

    WIP

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const weather = require( './data/weather.json' );
+const { response } = require('express');
 
 const app = express();
 app.use(cors());
@@ -10,16 +11,24 @@ const PORT = process.env.PORT || 3001;
 
 app.listen( PORT, ()=> console.log(`server is listening on port: ${PORT}`));
 
+
 app.get( '/weather', ( request, response ) => {
+  try{
+    let dailyForcast = weather.data.map( element => new DailyForcast( element.weather.description, element.valid_date));
 
-  // let city = weatherX.city_name;
-  // let lat = weatherX.lat;
-  // let lon = weatherX.lon;
+    response.json(dailyForcast);
+    // let city = weatherX.city_name;
+    // let lat = weatherX.lat;
+    // let lon = weatherX.lon;
 
-  let dailyForcast = weather.data.map( element => new DailyForcast( element.weather.description, element.valid_date));
+  } catch(error){
+    handleErrors(error, response);
+  }
+});
 
-  response.json(dailyForcast);
-} );
+function handleErrors(){
+  response.status(500).send('internal error');
+}
 
 class DailyForcast {
   constructor(description, date){
