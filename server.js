aa47de3e20936972f268d9c6e9e1c1a1ef866977
antoni <lorenzo.antonio.090@gmail.com>
2021-04-15 14:57:38 -0700

    create logic for successful api call to movie api using superagent

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-// const weather = require( './data/weather.json' );
 // const { response } = require('express');
 const superagent = require('superagent');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(cors());
+
+class DailyForecast {
+  constructor(description, date){
+    this.description = description;
+    this.date = date;
+  }
+}
+
 app.get( '/weather', ( request, response ) => {
   // pass params from front end for lat and lon based on city searched
   // setup consts for superagent
@@ -23,35 +30,62 @@ app.get( '/weather', ( request, response ) => {
     .get(url)
     .query(query)
     .then( results => {
-      // console.log('ln26 results.body.data: ', results.body.data); gives an array of day objects
       response.status(200).send(results.body.data.map( day => new DailyForecast( day.weather.description, day.valid_date )));
     }
     ).catch( error => {
-      console.log(error);
+      // console.log(error);
+      response.status(500).send('internal error', error);
     });
 });
 
-class DailyForecast {
-  constructor(description, date){
-    this.description = description;
-    this.date = date;
-  }
+// base url
+// https://api.themoviedb.org/3/search/movie
+
+// query params: api key, query
+// https://api.themoviedb.org/3/search/movie?api_key=7da92528f88d7ced5a0ecd25f1d5f992&query=seattle
+
+app.get('/movies', (request,response) => {
+
+  // city is hardcoded need the property passed from front end
+  // console.log('request.query',request.query);
+
+  const city = 'yuma';
+  const url = 'https://api.themoviedb.org/3/search/movie';
+  const query = {
+    api_key: process.env.REACT_APP_MOVIE_API_KEY,
+    query: city,
+  };
+
+  superagent
+    // .get(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_MOVIE_API_KEY}&query=seattle`)
+    .get(url)
+    .query(query)
+    .then( dataObject => {
+
+      // console.log('results.body: ',dataObject.body.results);
+
+      // dataObject.body.results is an array of objects, map here
+      response.status(200).send(dataObject.body.results
+        .map( movie => new RelatedMovies( movie ) ));
+      // response is an array of 17 objects with seven properties each
+    })
+    .catch( error => {
+      // console.log('catch',error);
+      response.status(500).send(error);
+    });
+
+} );
+
+// Constructor function for movie API Response
+function RelatedMovies(movie){
+  this.description = movie.overview;
+  this.popularity = movie.popularity;
+  this.poster = movie.poster_path;
+  this.released = movie.release_date;
+  this.title = movie.original_title;
+  this.votesTotal = movie.vote_count;
+  this.voteAverage = movie.vote_average;
 }
-// function handleErrors(){
-//   response.status(500).send('internal error');
-// }
 
+// listener
 app.listen( PORT, ()=> console.log(`server is listening on port: ${PORT}`));
-
-// response.status(200).send(queryResult.body.results.map( day => {
-//   new DailyForcast( day.weather.description, day.valid_date );
-// }));
-
-// }).catch(error => {
-//   handleErrors(error, response);
-// });
-// let dailyForcast = weather.data.map( element => new DailyForcast( element.weather.description, element.valid_date));
-// response.json(dailyForcast);
-// queryResult => {response.status(200).send(queryResult.body.data.map(
-//   day => {new DailyForcast(day.weather.description, day.valid_date);
-//   }));})
