4301be24fa224ee15d2e21d7734de437b6742e8d
antoni <lorenzo.antonio.090@gmail.com>
2021-04-16 15:58:18 -0700

    use base url, restrcture server js file

--- server.js
+++ server.js
@@ -4,15 +4,26 @@ const cors = require('cors');
 // const { response } = require('express');
 const superagent = require('superagent');
 const app = express();
-const PORT = process.env.PORT || 3001;
-
 app.use(cors());
 
+const PORT = process.env.PORT || 3001;
+
+// class for day objects
 class DailyForecast {
   constructor(description, date){
     this.description = description;
     this.date = date;
   }
+}
+// Constructor function for movie API Response
+function RelatedMovies(movie){
+  this.description = movie.overview;
+  this.popularity = movie.popularity;
+  this.poster = movie.poster_path;
+  this.released = movie.release_date;
+  this.title = movie.original_title;
+  this.votesTotal = movie.vote_count;
+  this.voteAverage = movie.vote_average;
 }
 
 app.get( '/weather', ( request, response ) => {
@@ -41,20 +52,17 @@ app.get( '/weather', ( request, response ) => {
 
 app.get('/movies', (request,response) => {
 
-  // city is hardcoded need the property passed from front end
-  console.log('request.query',request.query.citySearchTextField);
+  // console.log('request.query',request.query.citySearchTextField);
 
   const url = 'https://api.themoviedb.org/3/search/movie';
-
   const query = {
     api_key: process.env.REACT_APP_MOVIE_API_KEY,
     query: request.query.citySearchTextField,
   };
 
   superagent
-    .get(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_MOVIE_API_KEY}&query=${request.query.citySearchTextField}`)
-    // .get(url)
-    // .query(query)
+    .get(url)
+    .query(query)
     .then( dataObject => {
       console.log('results.body: ',dataObject.body);
       // dataObject.body.results is an array of objects, map here
@@ -68,17 +76,6 @@ app.get('/movies', (request,response) => {
     });
 
 } );
-
-// Constructor function for movie API Response
-function RelatedMovies(movie){
-  this.description = movie.overview;
-  this.popularity = movie.popularity;
-  this.poster = movie.poster_path;
-  this.released = movie.release_date;
-  this.title = movie.original_title;
-  this.votesTotal = movie.vote_count;
-  this.voteAverage = movie.vote_average;
-}
 
 // listener
 app.listen( PORT, ()=> console.log(`server is listening on port: ${PORT}`));
